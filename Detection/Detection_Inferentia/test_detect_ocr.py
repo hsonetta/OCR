a708f69b5e4fb3dad33006b77c6a2523f674d022
hsone <hsonetta@gmail.com>
2021-07-09 17:26:41 -0400

    Update dockerfile

--- Detection/Detection_Inferentia/test_detect_ocr.py
+++ Detection/Detection_Inferentia/test_detect_ocr.py
@@ -4,7 +4,7 @@ from neuronocr_compilation import easyocr
 from neuronocr_compilation.easyocr.detection import get_detector
 
 reader = easyocr.Reader(['en'], detector=True, recognizer=False, gpu=False, download_enabled=True, model_storage_directory='model_file', user_network_directory='user_network')
-network = get_detector(r'model_file\craft_mlt_25k.pth')
+network = get_detector(r'model_file/craft_mlt_25k.pth')
 image = 'images/facebook.jpg'
 result = reader.detect(image, net=network)
 print(result)
